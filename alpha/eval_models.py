cd093c949adf3d3ccba1b1e03aaaf17548fb2aa4
VLi <victorialiofficial@gmail.com>
2024-11-08 23:15:24 +0000

    gemini_results

--- alpha/eval_models.py
+++ alpha/eval_models.py
@@ -9,3 +9,7 @@ f.write('starcoder: ' + str(starcoder_results) + '\n')
 deepseek_results = start_evaluate('./deepseek_data_alpha.json')
 print('deepseek: ', deepseek_results)
 f.write('deepseek: ' + str(deepseek_results) + '\n')
+
+gemini_results = start_evaluate('./data_alpha.json')
+print('gemini: ', gemini_results)
+f.write('gemini: ' + str(gemini_results) + '\n')
