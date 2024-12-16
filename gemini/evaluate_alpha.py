4bb5a81be1d1288fb075c56e52c391c99ee7b70d
Wei Qiang <qiangwei@uw.edu>
2024-12-16 17:38:53 +0000

    update

--- gemini/evaluate_alpha.py
+++ gemini/evaluate_alpha.py
@@ -30,10 +30,39 @@ Here is the original function
 ``function begins``
 '''
 
+with open("./gemini/prompt_file_cot.txt", "r") as f:
+    few_shot_cot = json.load(f)
+    few_shot_cot_promt = ""
+    for data in few_shot_cot:
+        cot = data["step-by-step thoughts"]
+        changed = data["changed_function"]
+        original = data["original_function"]
+        name = data["function_name"]
+        argument = data["target_argument"]
+        change_to = data["change_to"]
+        few_shot_cot_promt += f'''Human: Given a python function {name} we want to replace the parameter '{argument}' with '{change_to}', with semantics and logics of the function preserved. 
+Here are some examples.
+``Example Begin``
+{original}
+
+Analyze the problems step by step and return the replaced function
+Assistant:
+{cot}
+
+returned function:
+{changed}
+``Example End``
+
+Here is the task function.
+``function begin``
+'''
+
 with open("./starcoder/prompt_file.txt", "r") as f:
-    few_shot_prompt = '''You are a code programmer, we would like you to perform a alpha-renaming task a given function, 
-changing a function argument name to another name and preserve the semantics. \n
-''' + f.read()+'\n'
+    few_shot_prompt = f'''You are a code programmer, we would like you to perform a alpha-renaming task a given function, 
+changing a function argument name to another name and preserve the semantics. 
+Here are some examples.
+{f.read()}
+'''
 
 def few_shot_evaluate(DATASET = "alpha/dataset/data_alpha_non_valid2.json"):
   # read dataset
@@ -46,12 +75,19 @@ def few_shot_evaluate(DATASET = "alpha/dataset/data_alpha_non_valid2.json"):
           target_argu = data["target_argument"]
           change_ar  = data["change_to"]
           original_function = data["original_function"]
-          expected = data["changed_function"]
+          #expected = data["changed_function"]
           name = data["function_name"]
           inputs = data["inputs"]
 
           prompt_task = f'''{few_shot_prompt}
+  Here is the original function, change 
   {original_function}
+  1.The argument we want you to change is '{target_argu}'
+  2.You need to change this argument to '{change_ar}' and preserve the semantics of the function.
+  3.Generate final answer using the following format.
+  {{
+  "changed_function": "function after changed"
+  }}
   '''
           response = model.generate_content(prompt_task, generation_config=generation_config)
           print(response.text)
@@ -63,9 +99,9 @@ def few_shot_evaluate(DATASET = "alpha/dataset/data_alpha_non_valid2.json"):
           accuracy = alpha.evaluate(original_function, changed_function, name, inputs)
           total_accuracy += accuracy
       
-  outfile = open("./alpha/evaluation_data/gemini_few_shot_data_alpha_non_valid2.json", 'w')
-  outfile.write(json.dumps(data_res))
-  outfile.close()
+  # outfile = open("./alpha/evaluation_data/gemini_few_shot_data_alpha_non_valid2.json", 'w')
+  # outfile.write(json.dumps(data_res))
+  # outfile.close()
 
   print("final accuracy: ", total_accuracy/total_count)
   return total_accuracy, total_count
@@ -86,16 +122,17 @@ def evaluate_with_cot(DATASET = "alpha/dataset/data_alpha_non_valid2.json"):
           name = data["function_name"]
           inputs = data["inputs"]
 
-          prompt_task = f'''{chain_of_thought_promt}
-  {original_function}
-  ``function ends``
-  1.The argument we want you to change is '{target_argu}'
-  2.You need to change this argument to '{change_ar}' and preserve the semantics of the function.
-  3.Produce your thoughts and generate final answer using the following format.
-  {{
+          prompt_task = f'''{few_shot_cot_promt}
+
+{original_function}
+``function ends``
+1.The argument we want you to change is '{target_argu}'
+2.You need to change this argument to '{change_ar}' and preserve the semantics of the function.
+3.Produce your thoughts and generate final answer using the following format.
+{{
   "step-by-step thoughts" : "1. first analyze the variables in conflicts 2. rename the variables.... 3. check if the after-change function preserve semantics",
   "changed_function": "function after changed"
-  }}
+}}
   '''
           response = model.generate_content(prompt_task, generation_config=generation_config)
           print(response.text)
